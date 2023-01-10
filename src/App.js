7c14ac766f923a195d974876d04411e77e63a5a7
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-10 22:11:09 +0600

    code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Home from "./Components/Home/Home.jsx";
 import Cart from "./Components/Cart/Cart.jsx";
 import './App.css';
+import { Route, Routes } from "react-router";
+import Nav from "./Components/Nav/Nav.jsx";
+import NotFound from "./Components/Nav/NotFound.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Cart></Cart>
+      {/* nav start */}
+      <Nav></Nav>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
